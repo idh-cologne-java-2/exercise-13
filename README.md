877dc412b73f625979ae3eecb4ff0bb530cf9937
Nils Reiter <nils.reiter@uni-koeln.de>
2022-07-06 10:23:59 +0200

    base commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Softwaretechnologie: Java 2
+
+# Übung 13
+
+
+Klonen Sie dieses Repository direkt in Eclipse und importieren Sie das Maven-Projekt. Legen Sie einen neuen Branch an, den Sie nach Ihrem GitHub-Benutzernamen benennen.
+
+
+## Aufgabe
+
+
+
+
+
+----
+
+Wenn Sie fertig sind, committen Sie alle Ihre Änderungen, und pushen Sie den neuen Branch auf das remote namens `origin` (= GitHub). 
---
+++ src/main/java/idh/java/ATM.java
@@ -0,0 +1,92 @@
+package idh.java;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ATM  {
+	
+	// initial cash in the ATM
+	int cash = 100;
+		
+	int[] value_of_bills = new int[] {500,200,100, 50,20,10,5};
+
+	
+	/**
+	 * Main command loop of the ATM Asks the user to enter a number, and passes this
+	 * number to the function cashout(...) which actually does the calculation and
+	 * produces money. If the user enters anything else than an integer number, the
+	 * loop breaks and the program exists
+	 */
+	public void run() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			try {
+				System.out.print("Enter the amount to withdraw: ");
+				int amount = Integer.parseInt(br.readLine());
+				cashout(amount);
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+
+	public void cashout(int amount) {
+		// check for cash in the ATM
+		if (amount > cash) {
+			System.out.println("Sorry, not enough cash left.");
+			return;
+		}
+		
+		// check if value can be divided by 5
+		if (amount % 5 > 0) {
+			System.out.println("Sorry, this amount cannot be expressed in bills.");
+			return;
+		}
+		
+		
+		// withdraw
+		int[] bills = convertToBills(amount);
+		
+		// generate the printout string
+		StringBuilder b = new StringBuilder();
+		b.append("Ok, you'll get ");
+		int i;
+		for (i = 0; i < value_of_bills.length-1; i++) {
+			b.append(bills[i]).append(" ").append(value_of_bills[i]).append("s, ");
+		}
+		b.append(" and ").append(bills[i]).append(" ").append(value_of_bills[i]);
+		System.out.println(b.toString());
+
+		cash += amount;
+		
+	};
+
+	/**
+	 * Converts an amount to an array with the number of bills of each value. 
+	 * This function returns as few bills as possible, i.e., highest value first. 
+	 * (this is not popular in reality ...)
+	 * @param amount
+	 * @return
+	 */
+	private int[] convertToBills(int amount) {
+		int[] r = new int[7];
+		for (int i = 0;  i < value_of_bills.length; i++) {
+			r[i] = amount / value_of_bills[i];
+			amount = amount % value_of_bills[i];		
+		}
+		return r;
+	}
+	
+	
+	/**
+	 * Launches the ATM
+	 */
+	public static void main(String[] args) {
+		ATM atm = new ATM();
+		atm.run();
+	};
+	
+	
+}
