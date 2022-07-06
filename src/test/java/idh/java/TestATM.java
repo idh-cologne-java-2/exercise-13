f22a19c68e9979b70cdb27f5962ec7d47e188ffc
Nils Reiter <nils.reiter@uni-koeln.de>
2022-07-06 10:39:57 +0200

    first test

---
+++ src/test/java/idh/java/TestATM.java
@@ -0,0 +1,21 @@
+package idh.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestATM {
+	
+	ATM atm;
+	
+	@BeforeEach
+	void setUp() {
+		atm = new ATM();
+	}
+	
+	@Test
+	void testConvertToBillsCorrectBehaviour() {
+		assertEquals(new int[] {0,0,0,0,1,1,1}, atm.convertToBills(35));
+	}
+}
