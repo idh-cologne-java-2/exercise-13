b4d6b38a1f1bf1654fc40f6a825fe47d93d51495
nonameich6 <nonameich6@penguin>
2022-07-08 18:28:42 +0200

    Finally

--- src/test/java/idh/java/TestConvertToBills.java
+++ src/test/java/idh/java/TestConvertToBills.java
@@ -1,7 +1,8 @@
 package idh.java;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +11,9 @@ import idh.java.ATM.IllegalInputException;
 public class TestConvertToBills {
 	
 	ATM r = new ATM();
-	int [][] e = {{1,0,0,0,1,1,1},{1,2,0,1,0,0,0},{0,1,1,0,0,1,1},{0,0,0,0,0,0,0}};
-	int [] a = {535, 950, 315, -15, 27};
+	int[] m = new int[7];
+	boolean b = false;
 	
-	//a = 535, e = {1,0,0,0,1,1,1}
-	//a = 950, e = {1,2,0,1,0,0,0}
-	//a = 315, e = {0,1,1,0,0,1,1}
-	//a = -15, e = {0,0,0,0,0,0,0}
-	//a = 27 , e = null
 
 	@Test
 	void test1() throws IllegalInputException {
@@ -26,21 +22,33 @@ public class TestConvertToBills {
 		//(d.h. es werden immer möglichst große Scheine ausgegeben).
 		//Testen Sie dies mit einer Reihe von möglichen Eingaben.
 		
-		assertEquals(e[0], r.convertToBills(a[0]));
-		assertEquals(e[1], r.convertToBills(a[1]));
-		assertEquals(e[2], r.convertToBills(a[2]));
+		m = r.convertToBills(535);
+		b = m[0] == 1 && m[1] == 0 && m[2] == 0 && m[3] == 0 && m[4] == 1 && m[5] == 1 && m[6] == 1;
+		assertTrue(b);
+		m = r.convertToBills(950);
+		b = m[0] == 1 && m[1] == 2 && m[2] == 0 && m[3] == 1 && m[4] == 0 && m[5] == 0 && m[6] == 0;
+		assertTrue(b);
+		m = r.convertToBills(315);
+		b = m[0] == 0 && m[1] == 1 && m[2] == 1 && m[3] == 0 && m[4] == 0 && m[5] == 1 && m[6] == 1;
+		assertTrue(b);
+		
+//		assertEquals(e[0], r.convertToBills(a[0]));
+//		assertEquals(e[1], r.convertToBills(a[1]));
+//		assertEquals(e[2], r.convertToBills(a[2]));
 	}
 	
 	@Test
 	void test2() throws IllegalInputException {
 		//Bei Eingabe einer negativen Zahl wird ein Array mit Nullen zurückgegeben.
-		assertEquals(e[3], r.convertToBills(a[3]));
+		m = r.convertToBills(-15);
+		b = m[0] == 0 && m[1] == 0 && m[2] == 0 && m[3] == 0 && m[4] == 0 && m[5] == 0 && m[6] == 0;
+		assertTrue(b);
 	}
 	
 	@Test
 	void test3() {
 		//Bei Eingabe einer Zahl die nicht durch fünf teilbar ist,
 		//wird eine Exception der Klasse IllegalInputException geworfen.
-		assertThrows(IllegalInputException.class, () -> { r.convertToBills(a[4]); });		
+		assertThrows(IllegalInputException.class, () -> { r.convertToBills(27); });		
 	}
 }
