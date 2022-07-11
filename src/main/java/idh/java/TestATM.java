d16de93acf4b90f9695d3cdc57f61f19d9b69f3b
timsc <timsc@LAPTOP-ESP68Q5G.speedport.ip>
2022-07-11 22:44:50 +0200

    Exercise-13

---
+++ src/main/java/idh/java/TestATM.java
@@ -0,0 +1,27 @@
+package idh.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import idh.java.ATM.IllegalInputException;
+
+public class ATMTest {
+
+	ATM atm = new ATM();
+	
+	@Test
+	public void testConvertToBills() {
+		Assert.assertThrows(IllegalInputException.class, () -> { atm.convertToBills(1); } );
+		
+		try {
+			Assert.assertArrayEquals(new int[] {0, 0, 0, 0, 0, 0, 0}, new ATM().convertToBills(-20));
+			Assert.assertArrayEquals(new int[] {1, 0, 0, 0, 1, 0, 0}, new ATM().convertToBills(520));
+			Assert.assertArrayEquals(new int[] {1, 1, 1, 0, 0, 0, 0}, new ATM().convertToBills(800));
+			Assert.assertArrayEquals(new int[] {1, 2, 0, 1, 0, 0, 0}, new ATM().convertToBills(950));
+		}
+		catch (IllegalInputException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
