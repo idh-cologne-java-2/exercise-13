a3fbc3486904b6d34d0bf0b9a5d82b1869d4ea69
Nils Reiter <nils.reiter@uni-koeln.de>
2022-07-13 09:28:45 +0200

    test implementations

--- src/test/java/idh/java/TestATM.java
+++ src/test/java/idh/java/TestATM.java
@@ -1,6 +1,7 @@
 package idh.java;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,5 +20,19 @@ public class TestATM {
 	@Test
 	void testConvertToBillsCorrectBehaviour() throws IllegalInputException {
 		assertArrayEquals(new int[] {0,0,0,0,1,1,1}, atm.convertToBills(35));
+		assertArrayEquals(new int[] {20,0,0,0,0,0,1}, atm.convertToBills(10005));
+		assertArrayEquals(new int[] {0,0,0,1,1,1,0}, atm.convertToBills(80));
+		assertArrayEquals(new int[] {0,0,1,0,1,0,0}, atm.convertToBills(120));
 	}
+	
+	@Test
+	void testConvertToBillsInputZero() {
+		assertThrows(IllegalInputException.class,  () -> atm.convertToBills(7));
+	}
+
+	@Test
+	void testConvertToBillsInputNegative() throws IllegalInputException {
+		assertArrayEquals(new int[] {0,0,0,0,0,0,0}, atm.convertToBills(-5));
+	}
+
 }
