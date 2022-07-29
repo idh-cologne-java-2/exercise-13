f51590862609cd5571e9a27c75a266629d144432
ahxpe <amelie.hupe@arcor.de>
2022-07-29 23:40:37 +0200

    exercise13

---
+++ src/main/java/idh/java/Test.java
@@ -0,0 +1,29 @@
+package idh.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import idh.java.ATM.IllegalInputException;
+
+class Test {
+	ATM atm = new ATM();
+
+	@Test++++
+	
+	void testConvertToBills() {
+		assertThrows(IllegalInputException.class, () -> {
+			atm.convertToBills(1);
+		});
+		try {
+			assertArrayEquals(new int[] {0,0,0,0,0,0,0}, atm.convertToBills(-1));
+			assertArrayEquals(new int[] {2,0,0,0,0,0,1}, atm.convertToBills(1005));
+			assertArrayEquals(new int[] {1,2,0,0,1,0,0}, atm.convertToBills(920));
+			assertArrayEquals(new int[] {1,1,1,1,1,1,1}, atm.convertToBills(885));
+		} catch (IllegalInputException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
